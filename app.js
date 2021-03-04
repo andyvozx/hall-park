25e0037a0e405cad319000711231b13a41c755c9
Andy Vo <andyvozx@gmail.com>
2021-03-04 07:32:14 -0600

    remove bounds pn map

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 33.10324394465482, lng: -96.82622539804454 },
     zoom: 16.8,
-    restriction: {
-      latLngBounds: HALL_BOUNDS,
-      strictBounds: false,
-    },
+    // restriction: {
+    //   latLngBounds: HALL_BOUNDS,
+    //   strictBounds: false,
+    // },
   });
 }
 
